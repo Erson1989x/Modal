ea65ddb8362e6b5382032e591da8c0eeaad9f101
Erhard Nagy <Erson1989x@gmail.com>
2024-05-17 22:38:33 +0300

    modal

---
+++ script.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const buttonCloseModal = document.querySelector(".close-modal");
+const buttonOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < buttonOpenModal.length; i++) {
+    console.log(buttonOpenModal[i].textContent);
+}
+
+const openButton = () => {
+    
+     modal.classList.remove('hidden')
+     overlay.classList.remove('hidden')
+}
+
+const closeButton = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+
+buttonOpenModal.forEach((btn) => btn.addEventListener('click', openButton));
+buttonCloseModal.addEventListener('click', closeButton);
+overlay.addEventListener('click', closeButton);
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeButton();
+    }
+})
